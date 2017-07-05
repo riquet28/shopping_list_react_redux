b8fee24ebd6dfc5fd478c2330846494943296196
riquet28 <eric.merlin3@gmail.com>
2017-07-05 03:49:39 +0200

    Gestion submission form

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,14 +6,30 @@ class Form extends React.Component {
     quantity: 0
   };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    this.props.addArticle(this.state);
+    this.setState({ name: '', quantity: 0 });
+  };
+
   render() {
     return(
       <div>
-        <h3>Ajouter des articles à acheter</h3>
-        <form>
-          <input type="number" placeholder="Quantité"
-            value={this.state.quantity} onChange={(event) => this.setState({ quantity: event.target.value })} />
-          <input type="text" placeholder="Article" value={this.state.name} onChange={(event) => this.setState({ name: event.target.value })} />
+        <h3>{this.props.formTitle}</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="number"
+            placeholder="Quantité"
+            value={this.state.quantity}
+            onChange={(event) => this.setState({ quantity: event.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Article"
+            value={this.state.name}
+            onChange={(event) => this.setState({ name: event.target.value })}
+          />
           <button type="submit">Ajouter</button>
         </form>
       </div>
