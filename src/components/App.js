52f278f8e197b0647c5b7f54b6b8b525d0def926
riquet28 <eric.merlin3@gmail.com>
2017-07-05 02:28:19 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+      return (
+        <div>
+          <h3>Liste de courses</h3>
+          <div>En construction</div>
+        </div>
+      )
+    }
+}
+
+export default App;
