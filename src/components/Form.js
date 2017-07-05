9ca7e2e977d5fa57949d3a7c1127ed3f52cd4910
riquet28 <eric.merlin3@gmail.com>
2017-07-05 03:12:42 +0200

    Modification state component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const Form = () => {
-  return (
-    <div>Form component</div>
-  );
-};
+class Form extends React.Component {
+  state = {
+    name: '',
+    quantity: 0
+  };
+
+  render() {
+    return(
+      <div>
+        <h3>Ajouter des articles à acheter</h3>
+        <form>
+          <input type="number" placeholder="Quantité"
+            value={this.state.quantity} onChange={(event) => this.setState({ quantity: event.target.value })} />
+          <input type="text" placeholder="Article" value={this.state.name} onChange={(event) => this.setState({ name: event.target.value })} />
+          <button type="submit">Ajouter</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Form;
