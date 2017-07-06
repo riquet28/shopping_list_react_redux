c9a1ef114634680c12c75f6a3a483b7ce8573d37
riquet28 <eric.merlin3@gmail.com>
2017-07-06 02:13:58 +0200

    Connection to react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Form from './Form';
 import ItemList from './ItemList';
+import { connect } from 'react-redux';
 
 class App extends React.Component {
 
@@ -9,22 +10,37 @@ class App extends React.Component {
     articles: []
   }
 
-  addArticle = (article) => {
-    let oldArticles = this.state.articles;
-    article.id = Date.now();
-    let newArticles = [...oldArticles, article];
-    this.setState({ articles: newArticles })
-  }
-
   render() {
     return (
       <div>
         <h3>Liste de courses</h3>
-        < Form formTitle="Ajouter un article" addArticle={this.addArticle}  />
-        < ItemList articles={this.state.articles}/>
+        < Form formTitle="Ajouter un article" addArticle={this.props.addArticle}  />
+        < ItemList articles={this.props.articles}/>
       </div>
     )
   }
+} // end class
+
+const addArticleActionCreator = (article) => {
+  return {
+    type: 'ADD_ARTICLE',
+    payload: article
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {
+    articles: state.articles
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addArticle: (article) => {
+      dispatch(addArticleActionCreator(article));
+    }
+  }
 }
 
-export default App;
+// export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const articlesReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_ARTICLE':
+      console.log('ADD_ARTICLE');
+      console.log('action', action);
+      action.payload.id = Date.now();
+      const newState = [...state, action.payload];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(combineReducers({articles: articlesReducer}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+window.store = store;
+
+ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
